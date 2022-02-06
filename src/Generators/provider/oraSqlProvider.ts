f0176b92cb2084fafc1347563a011464699700ec
Ioan ALferaru <ioan.alferaru@gmail.com>
2022-02-06 18:39:52 +0100

    feat(ora): oracle sql provider

--- src/Generators/provider/oraSqlProvider.ts
+++ src/Generators/provider/oraSqlProvider.ts
@@ -11,8 +11,30 @@ class PgSqlProvider implements IDBProvider {
     connection() {
         let conn = `const oracledb = require('oracledb')();\n`;
         conn += `const connect = {};\n`;
+        conn += `const parseUrl = require('url-parse/);'\n`;
         conn += `// WARNING - Properly secure the connection string\n`;
-        conn += `connect.conn = oracledb.open('${this.connString}');\n`;
+        conn += `const connString='${this.connString}');
+const url = parse(connString, true);
+let db
+try {
+    connect.conn = await oracledb.getConnection( {
+      user          : url.username,
+      password      : url.password,
+      connectString : \`\${url.host}:\${url.port}/\${url.pathname}?\${url.query}\`,
+    });
+
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connect.conn) {
+      try {
+        await connect.conn.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+`;
 
         return conn;
     }
@@ -21,8 +43,8 @@ class PgSqlProvider implements IDBProvider {
         let query = `const sql = 'SELECT * FROM "${table}" WHERE "${col}" = $1';\n`;
 
         returnsMany ?
-            query += `${tab.repeat(4)}return connect.conn.many(sql, ${val})\n` :
-            query += `${tab.repeat(4)}return connect.conn.one(sql, ${val})\n`;
+            query += `${tab.repeat(4)}return connect.conn.execute(sql, ${val})\n` :
+            query += `${tab.repeat(4)}return connect.conn.execute(sql, ${val})\n`;
 
         query += addPromiseResolution();
 
@@ -31,7 +53,7 @@ class PgSqlProvider implements IDBProvider {
 
     select(table: string) {
         let query = `const sql = 'SELECT * FROM "${table}"';\n`;
-        query += `${tab.repeat(4)}return connect.conn.many(sql)\n`;
+        query += `${tab.repeat(4)}return connect.conn.execute(sql)\n`;
 
         query += addPromiseResolution();
 
@@ -46,7 +68,7 @@ class PgSqlProvider implements IDBProvider {
         const params = normalized.map((val, idx) => `$${idx + 1}`).join(', ');
 
         let query = `const sql = 'INSERT INTO "${table}" (${cols}) VALUES (${params}) RETURNING *';\n`;
-        query += `${tab.repeat(4)}return connect.conn.one(sql, [${normalized.join(
+        query += `${tab.repeat(4)}return connect.conn.execute(sql, [${normalized.join(
             ', '
         )}])\n`;
 
@@ -59,7 +81,7 @@ class PgSqlProvider implements IDBProvider {
         let query = `const sql = \`UPDATE "${table}" SET \${parameterized} WHERE "${idColumnName}" = $1 RETURNING *\`;\n`;
         query += `${tab.repeat(
             4
-        )}return connect.conn.one(sql, [${idColumnName}, ...Object.values(rest)])\n`;
+        )}return connect.conn.execute(sql, [${idColumnName}, ...Object.values(rest)])\n`;
 
         query += addPromiseResolution();
         return query;
@@ -67,7 +89,7 @@ class PgSqlProvider implements IDBProvider {
 
     delete(table: string, column: string) {
         let query = `const sql = 'DELETE FROM "${table}" WHERE "${column}" = $1 RETURNING *';\n`;
-        query += `${tab.repeat(4)}return connect.conn.one(sql, args.${column})\n`;
+        query += `${tab.repeat(4)}return connect.conn.execute(sql, args.${column})\n`;
 
         query += addPromiseResolution();
 
